4936d6ab9da1551ce51ec4096661732a606ec843
sahanbcs <sahanepic@gmail.com>
2021-05-19 16:36:43 +0530

    first commit

---
+++ src/main/java/com/sahanbcs/DocspringkubApplication.java
@@ -0,0 +1,13 @@
+package com.sahanbcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocspringkubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocspringkubApplication.class, args);
+	}
+
+}
